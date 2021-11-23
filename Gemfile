df6d1a14ebba15fd9d599b1dce7bd38774a06be9
Lisa <redlisa125@gmail.com>
2021-11-22 16:39:41 -1000

    commit tree d26c253bba6be5e180bcd752478cc61d2272c66f

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :development, :test do
-  gem 'rspec-rails', '~> 5.0.0'
+  gem 'rspec-rails'
 end
 
 group :test do
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
-  def index
-    students = Student.all
-    render json: students
+def index
+  if(params[:name])
+  student = Student.where("first_name =? or last_name =?", params[:name].capitalize, params[:name].capitalize)
+  render json: students
+  else
+  students = Student.all
+  render json: student
   end
+end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+  
 end
